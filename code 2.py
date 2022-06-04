ee3a20fd9a97293d98e1ccf2aa4ebe027e978205
plwanejexperto <65378345+plwanejexperto@users.noreply.github.com>
2022-06-04 13:56:58 +0800

    Update and rename code2.py to code 2.py

---
+++ code 2.py
@@ -0,0 +1,3 @@
+hexa = 'a5839f87f0cc7409842a238aa28be2d051d9b948'
+deci = int(hexa,16)
+print(deci)
--- code2.py
+++
@@ -1,3 +0,0 @@
-hexa = '0xa5839F87f0cC7409842a238aA28BE2D051D9B948'
-deci = int(hexa,16)
-print(deci)
